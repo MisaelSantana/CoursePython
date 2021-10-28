18e357ea1e85a28c687e10d61115753b51f5ad26
MisaelSantana <misaelsantana2011hotmail.com>
2021-10-28 17:42:51 -0300

    add module 04

---
+++ Section04 - Variables and Type Data/TypeNumericFloat.py
@@ -0,0 +1,17 @@
+"""
+Tipo Float:
+
+# Tipo real/decimal;
+# Casas decimais;
+OBS: O separador de casas decimais na programação é o ponto e não a vírgula.
+"""
+
+# Errado:
+valor = 1, 44
+print(f'{valor}')
+print(type(valor))
+
+# Certo:
+valor = 1.44
+print(f'{valor}')
+print(type(valor))
