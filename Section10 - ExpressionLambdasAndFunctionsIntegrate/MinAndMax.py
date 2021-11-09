63db62fe7898d0cb502fadfe19c8ed678231c58c
MisaelSantana <misaelsantana2011hotmail.com>
2021-11-09 16:32:45 -0300

    Commit section 12

--- Section10 - ExpressionLambdasAndFunctionsIntegrate/MinAndMax.py
+++ Section10 - ExpressionLambdasAndFunctionsIntegrate/MinAndMax.py
@@ -38,3 +38,65 @@ print(min(dict))  # a
 # Imprimindo os valores:
 print(max(dict.values()))  # 129
 print(min(dict.values()))  # 4
+
+
+# Faça um programa que receba dois valores do usuário e mostre o maior:
+val1= int(input('Informe o primeiro valor'))
+val2= int(input('Informe o segundo valor'))
+
+print(max(val1, val2))
+
+
+# Exemplo de uso do min():
+
+# Faça um programa que receba dois valores do usuário e mostre o menor valor:
+val1= int(input('Informe o primeiro valor'))
+val2= int(input('Informe o segundo valor'))
+
+print(min(val1, val2))
+
+
+# Outros exemplos:
+nomes = ['Arya', 'Samson', 'Dora', 'Tim', 'Ollivander',]
+
+print(max(nomes))  # Tim
+print(min(nomes))  # Arya
+
+print(max(nomes, key=lambda nome: len(nome)))  # Ollivander
+print(min(nomes, key=lambda nome: len(nome)))  # Tim
+
+
+# Exemplo:
+musicas = [
+    {"titulo": "Thunderstruck", "tocou": 3},    
+    {"titulo": "Dead Skin Mask", "tocou": 2},    
+    {"titulo": "Back in Black", "tocou": 4},    
+    {"titulo": "Too old to rock in roll, too ynoung to die", "tocou": 32}    
+]
+
+print(max(musicas, key=lambda musica: musica['tocou']))
+print(min(musicas, key=lambda musica: musica['tocou']))
+
+# Imprimar apenas o nome:
+print(max(musicas, key=lambda musica: musica['tocou'])['titulo'])
+print(min(musicas, key=lambda musica: musica['tocou'])['titulo'])
+
+# Encontrar a música mais tocada e menos tocada sem utilizar o min(), max() e lambda:
+max = 0
+for musica in musicas:
+    if musica['tocou'] > max:
+        max = musica['tocou']
+
+for musica in musicas:
+    if musica['tocou'] == max:
+        print(musica['titulo'])
+
+
+min = 0
+for musica in musicas:
+    if musica['tocou'] < min:
+        min = musica['tocou']
+
+for musica in musicas:
+    if musica['tocou'] == min:
+        print(musica['titulo'])
