7320ece70824729842844cb17c45df48ecd77fc5
MisaelSantana <misaelsantana2011hotmail.com>
2021-11-10 13:17:02 -0300

    create project python in project exist and commit att to section 12

--- Section08 - Functions in Python/FunctionWithParameter.py
+++ Section08 - Functions in Python/FunctionWithParameter.py
@@ -108,14 +108,15 @@ print(nome_completo(sobrenome='Santana', nome='Misael'))
 # Erro comum na utilização do return:
 
 def soma_impares(numeros):
+    """Função utilizada para realizar a soma."""
     total = 0
     for num in numeros:
         if num % 2 != 0:
             total = total + num
         return total
 
-lista = [1, 2, 3, 4, 5, 6, 7]
-print(soma_impares(lista))
+#lista = [1, 2, 3, 4, 5, 6, 7]
+#print(soma_impares(lista))
 
-tupla = (1, 2, 3, 4, 5, 6, 7)
-print(soma_impares(tupla))
+#tupla = (1, 2, 3, 4, 5, 6, 7)
+#print(soma_impares(tupla))
