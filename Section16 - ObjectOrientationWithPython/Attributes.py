0f1dad921e8c45af6be479fd31cb007000eb551f
MisaelSantana <misaelsantana2011hotmail.com>
2021-11-19 11:19:48 -0300

    commit section 15 and part section 16

---
+++ Section16 - ObjectOrientationWithPython/Attributes.py
@@ -0,0 +1,23 @@
+"""
+Programação Orientada a Objetos: Atributos
+
+# Atributos:
+-> Representamos as características do objeto, ou seja, pelos atributos nós
+   conseguimos representar computacionalmente os estados de um objeto.
+
+# Em Python, dividimos os atributos em 3 grupos:
+-> Atributos de Instância;
+-> Atributos de Classe;
+-> Atributos Dinâmicos;
+
+# Atributos de instância:
+-> São atributos declarados dentro do método construtor.
+
+
+# OBS:
+-> Método Construtor é um método especial utilizado para construção do objeto.
+
+"""
+
+
+# 
