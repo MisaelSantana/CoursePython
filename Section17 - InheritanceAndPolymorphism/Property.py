f1c535883b84d165cc0d4175a3b057cff7d25a6d
MisaelSantana <misaelsantana2011hotmail.com>
2021-11-22 15:15:07 -0300

    commit section 16

---
+++ Section17 - InheritanceAndPolymorphism/Property.py
@@ -0,0 +1,32 @@
+"""
+POO - Propriedades(Properties):
+
+# 
+"""
+
+
+# Exemplo:
+
+class Conta:
+
+    contador = 0
+
+    def __init__(self, titular, saldo, limite):
+        self.__numero = Conta.contador + 1
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        Conta.contador += 1
+
+    def extrato(self):
+        return(f'Saldo de {self.__saldo} do cliente {self.__titular}')
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.__saldo -= valor
+        destino.__saldo += valor
