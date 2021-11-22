6c1107cfa09284f9425713d8a4da7248f43a0144
MisaelSantana <misaelsantana2011hotmail.com>
2021-11-22 16:55:59 -0300

    commit section 17

---
+++ Section17 - InheritanceAndPolymorphism/SuperMethod.py
@@ -0,0 +1,29 @@
+"""
+POO - Método 'super()'
+
+# O método 'super()' se refere à super classe.
+"""
+
+
+# Relembrando:
+class Animal:
+
+    def __init__(self, nome, especie):
+        self.__nome = nome
+        self.__especie = especie
+
+        def faz_som(self, som):
+            print(f'O {self.__nome} fala {som}')
+
+class Gato(Animal):
+
+    def __init__(self, nome, especie, raca):
+        #Animal.__init__(self, nome, especie)
+        super().__init__(nome, especie)
+        self.__raca = raca
+
+
+# Testando a classe:
+felix = Gato('Felix', 'Felino', 'Angorá')
+print(felix)
+felix.faz_som('miau')
