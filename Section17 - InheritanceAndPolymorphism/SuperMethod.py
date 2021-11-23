32bc1298d9621a4732a3fc229c04e8ec37dcd117
MisaelSantana <misaelsantana2011hotmail.com>
2021-11-23 10:38:37 -0300

    Commit section 17

--- Section17 - InheritanceAndPolymorphism/SuperMethod.py
+++ Section17 - InheritanceAndPolymorphism/SuperMethod.py
@@ -12,18 +12,19 @@ class Animal:
         self.__nome = nome
         self.__especie = especie
 
-        def faz_som(self, som):
-            print(f'O {self.__nome} fala {som}')
+    def faz_som(self, som):
+        print(f'O {self.__nome} fala {som}')
 
 class Gato(Animal):
 
     def __init__(self, nome, especie, raca):
-        #Animal.__init__(self, nome, especie)
+        # Animal.__init__(self, nome, especie)
         super().__init__(nome, especie)
+        # O super serve para acessar qualquer elemento da classe pai
+        super().faz_som('Auauauuaua')
         self.__raca = raca
 
 
 # Testando a classe:
 felix = Gato('Felix', 'Felino', 'Angorá')
-print(felix)
 felix.faz_som('miau')
